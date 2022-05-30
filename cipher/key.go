b18c130f9beac0496621674e3e668abf07127eba
Priyanshu Jain <ipriyanshujain@gmail.com>
2022-05-30 09:42:06 +0530

    feat: add key generation scheme with given charset

---
+++ cipher/key.go
@@ -0,0 +1,31 @@
+package cipher
+
+import (
+	"crypto/rand"
+	"io"
+	"math/big"
+)
+
+// generate a variable sized byte random key using
+func GenEncryptionKey(size int) (*[]byte, error) {
+	key := make([]byte, size)
+	_, err := io.ReadFull(rand.Reader, key[:])
+	if err != nil {
+		return nil, err
+	}
+	return &key, nil
+}
+
+// generate a variable sized byte random key using a given character set
+// example charset "0123456789" when only digits are required
+func GenEncryptionKeyWithCharset(charset string, size int) (*[]byte, error) {
+	iv := make([]byte, size)
+	for i := range iv {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return nil, err
+		}
+		iv[i] = charset[num.Int64()]
+	}
+	return &iv, nil
+}
---
+++ cipher/key_test.go
@@ -0,0 +1,16 @@
+package cipher
+
+import (
+	"testing"
+)
+
+func TestGenEncryptionKey(t *testing.T) {
+	key, err := GenEncryptionKey(32)
+	if err != nil {
+		t.Errorf("GenEncryptionKey() error = %v", err)
+		return
+	}
+	if len(*key) != 32 {
+		t.Errorf("GenEncryptionKey() = %v, want %v", len(*key), 32)
+	}
+}
